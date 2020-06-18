97473a09ea18dad6924944410f86ddd56818983b
Kakashish <ashish.dwivedi@st.niituniversity.in>
2020-06-18 11:14:49 +0530

    config changed

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
 <?php
 	
 	//default database configuration
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "foodshaladb";
+	//$servername = "localhost";
+	//$username = "root";
+	//$password = "";
+	//$dbname = "foodshaladb";
+
+
+
+	//online hsoting database configuration
+	$servername = "fdb24.awardspace.net";
+	$username = "3478461_foodshaladb";
+	$password = "windows@2";
+	$dbname = "3478461_foodshaladb";
 
 ?>
